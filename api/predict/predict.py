79e2c716281c9511169b5d8169701316c202adad
bordisthom <77927906+bordisthom@users.noreply.github.com>
2022-12-14 21:20:41 +0100

    Add files via upload

--- api/predict/predict.py
+++ api/predict/predict.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, status
 from models.wine import Wine
+from wine_prediction import WinePrediction
 
 predict_router = APIRouter()
 
+wine_prediction = WinePrediction()
+
+# ok
 @predict_router.post("/api/predict", status_code=status.HTTP_201_CREATED)
 async def rate_wine(wine: Wine):
-    quality = 9.2
-    return {"Quality": quality}
+    return wine_prediction.predictWine(wine)
 
 @predict_router.get("/api/predict")
 async def get_perfect_wine_characteristics():
