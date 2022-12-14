f2c0db1315d5f5e7df3a266f5902a7382bcd0c75
bordisthom <77927906+bordisthom@users.noreply.github.com>
2022-12-14 21:20:12 +0100

    Add files via upload

--- api/model/model.py
+++ api/model/model.py
@@ -1,20 +1,29 @@
 from fastapi import APIRouter, status
 from models.wine import Wine
+from fastapi.responses import FileResponse
+from wine_prediction import WinePrediction
+import wget
 
 model_router = APIRouter()
 
+wine_prediction = WinePrediction()
+
 @model_router.get("/api/model")
 async def get_serialized_model():
-    return {"test": None}
+    #wget.download('./rf.joblib')
+    return FileResponse(path= 'rf.joblib', filename= 'rf.joblib', media_type='joblib')
+    #return FileResponse('rf.joblib')
 
 @model_router.get("/api/model/description")
 async def get_model_description():
     return {"description": "params"}
 
+# ok
 @model_router.put("/api/model")
 async def add_wine(wine: Wine):
-    return {"message": "wine added"}
+    return wine_prediction.addWine(wine)
 
+# ok
 @model_router.post("/api/model/retrain", status_code=status.HTTP_201_CREATED)
 async def retrain_model():
-    return {"message": "model retrained"}
+    return wine_prediction.retrain()
