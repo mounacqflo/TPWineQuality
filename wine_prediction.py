1f9532b1f80245957be26bae0c70f9da0445287f
bordisthom <77927906+bordisthom@users.noreply.github.com>
2022-12-18 21:07:44 +0100

    Update wine_prediction.py

--- wine_prediction.py
+++ wine_prediction.py
@@ -80,7 +80,7 @@ class WinePrediction:
         X = data.drop('quality', axis=1)
         y = data['quality']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
-        random_forest = RandomForestClassifier(n_estimators=400)
+        random_forest = RandomForestClassifier(n_estimators=300)
         random_forest.fit(X_train, y_train)
         joblib.dump(random_forest, "./rfc.joblib")
         return {"message": "Successful retrain."}
